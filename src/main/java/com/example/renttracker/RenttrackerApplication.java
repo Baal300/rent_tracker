503ee62a45730a5edcd8e6614d91b370767a7868
Dat Do <duytiendatdo@yahoo.de>
2025-11-28 03:34:50 +0100

    Initialize project

---
+++ src/main/java/com/example/renttracker/RenttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.renttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenttrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenttrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/renttracker/RenttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenttrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
